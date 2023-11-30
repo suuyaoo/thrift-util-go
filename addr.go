60a8d2d59efea76535977c12478a42faead4e502
suuyaoo <suuyaoo@gmail.com>
2023-11-30 15:43:40 +0800

    add address invalid check

--- addr.go
+++ addr.go
@@ -1,5 +1,11 @@
 package thriftutil
 
+import (
+	"net"
+	"strconv"
+	"strings"
+)
+
 func ThriftAddress(addr string) string {
 	if len(addr) < 7 {
 		return addr
@@ -16,18 +22,56 @@ func ThriftAddress(addr string) string {
 	return addr
 }
 
-func IsThriftAddress(addr string) bool {
-	if len(addr) < 7 {
+func checkAddress(prep, addr string) bool {
+	pos := strings.LastIndex(addr, ":")
+	if pos == -1 {
 		return false
 	}
+	prepStr := ""
+	ipStr := addr[0:pos]
+	portStr := addr[pos+1:]
+	if strings.Contains(ipStr, ".") {
+		prepStr = "tcp4://"
+	} else if strings.Contains(ipStr, ":") {
+		prepStr = "tcp6://"
+		if ipStr[0] == '[' {
+			ipLen := len(ipStr)
+			if ipStr[ipLen-1] == ']' {
+				ipStr = ipStr[1 : ipLen-2]
+			} else {
+				return false
+			}
+		}
+	}
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		return false
+	}
+	if prep != "" && prepStr != prep {
+		return false
+	}
+	port, err := strconv.ParseInt(portStr, 10, 16)
+	if err != nil {
+		return false
+	}
+	if port < 0 {
+		return false
+	}
+	return true
+}
+
+func IsThriftAddress(addr string) bool {
+	if len(addr) < 7 {
+		return checkAddress("", addr)
+	}
 	if addr[0:7] == "http://" {
-		return true
+		return checkAddress("", addr[7:])
 	}
 	if addr[0:7] == "tcp4://" {
-		return true
+		return checkAddress("tcp4://", addr[7:])
 	}
 	if addr[0:7] == "tcp6://" {
-		return true
+		return checkAddress("tcp6://", addr[7:])
 	}
-	return false
+	return checkAddress("", addr)
 }
---
+++ addr_test.go
@@ -0,0 +1,53 @@
+package thriftutil
+
+import "testing"
+
+func TestThriftAddress(t *testing.T) {
+	if !IsThriftAddress("tcp4://127.0.0.1:8888") {
+		t.Error("tcp4://127.0.0.1:8888")
+	}
+
+	if !IsThriftAddress("tcp6://[::1]:8888") {
+		t.Error("tcp6://[::1]:8888")
+	}
+
+	if !IsThriftAddress("http://127.0.0.1:8888") {
+		t.Error("http://127.0.0.1:8888")
+	}
+
+	if !IsThriftAddress("http://[::1]:8888") {
+		t.Error("http://[::1]:8888")
+	}
+
+	if !IsThriftAddress("127.0.0.1:8888") {
+		t.Error("127.0.0.1:8888")
+	}
+
+	if !IsThriftAddress("[::1]:8888") {
+		t.Error("[::1]:8888")
+	}
+
+	if IsThriftAddress("8888") {
+		t.Error("8888")
+	}
+
+	if IsThriftAddress("abc") {
+		t.Error("abc")
+	}
+
+	if IsThriftAddress("tcp6://[::1:9999") {
+		t.Error("tcp6://[::1:9999")
+	}
+
+	if IsThriftAddress("tcp6://::1]:9999") {
+		t.Error("tcp6://::1]:9999")
+	}
+
+	if IsThriftAddress("tcp6://127.0.0.1:9999") {
+		t.Error("tcp6://127.0.0.1:9999")
+	}
+
+	if IsThriftAddress("tcp4://[::1]:8888") {
+		t.Error("tcp4://[::1]:8888")
+	}
+}
